ce0a1c21fd86cb0a7cab87e329f0e5521ed204ae
DiegoZz <dfflores@espol.edu.ec>
2023-05-25 21:59:11 -0500

    segundo commit

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/SnakeModel.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/SnakeModel.java
@@ -55,7 +55,7 @@ public class SnakeModel extends GameModel {
 	private static final GameTile SNAKE_TILE = new RectangularTile(Color.darkGray);
 
 	/** Graphical representation of the snake's head */
-	private static final GameTile SNAKE_HEAD_TILE = new RectangularTile(Color.GRAY);
+	private static final GameTile SNAKE_HEAD_TILE = new RectangularTile(Color.LIGHT_GRAY);
 	
 	/** Graphical representation of a blank tile. */
 	private static final GameTile BLANK_TILE = new GameTile();
